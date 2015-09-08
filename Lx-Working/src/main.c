59a0a451aec42eee858f3f174127c900c82c6bbb
vav <l.e.h.a.vav@gmail.com>
2015-09-08 20:03:06 +0300

    working wifi

--- Lx-Working/src/main.c
+++ Lx-Working/src/main.c
@@ -2,169 +2,254 @@
 #include "stm32f4xx_hal.h"
 #include <stdint.h>
 #include "step_utils.h"
+#include <stdio.h>
+#include <string.h>
 
-void switchState();
-void checkBtn(int);
+GPIO_InitTypeDef GPIOG_Init;
 
-// состояние изменяется в обработчике прерывания, а в главном цикле лишь зажигаются светодиоды определенным образом в зависимости от состояния
-volatile int index   = 0;
-volatile int error   = 0;
-volatile int code[4] = {1, 3, 1, 2};
+UART_HandleTypeDef huart1;
+ADC_HandleTypeDef hadc1;
+RingBuffer rBuffer;
 
-void Led_Init()
+uint32_t ADC_GetValue(uint32_t adc_channel);
+
+void UART_Init()
 {
-    // структуры инициализации портов и их пинов
-    GPIO_InitTypeDef GPIOG_Init;
+    GPIO_InitTypeDef GPIO_InitStruct;
 
+    __GPIOA_CLK_ENABLE();
+    __USART1_CLK_ENABLE();
+
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 9600;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    HAL_UART_Init(&huart1);
+
+    NVIC_EnableIRQ(USART1_IRQn); // Включаем прерывания UART1
+    __HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
+}
+
+void ADC1_Init(void)
+{
+    GPIO_InitTypeDef portCinit;
+    __ADC1_CLK_ENABLE();
+    __GPIOC_CLK_ENABLE();
+
+    /**
+    ADC1 GPIO Configuration
+    Из документации прочитали, что
+    пин PC0 используется, как 10 канал для АЦП1
+    PC1 - 11
+    PC2 - 12
+    PC3 - 13
+    */
+    portCinit.Pin   = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3;
+    portCinit.Mode  = GPIO_MODE_ANALOG; // Выставляем его в аналоговый режим
+    portCinit.Pull  = GPIO_NOPULL; // без подтягивающих резисторов
+    HAL_GPIO_Init(GPIOC, &portCinit); // инициализируем порт С
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+    */
+    hadc1.Instance = ADC1; // указываем какой АЦП хотим использовать. Варианты ADC1, ADC2, ADC3
+    hadc1.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2; // Делитель частоты для АЦП
+    hadc1.Init.Resolution = ADC_RESOLUTION12b; // точность (разрешение) АЦП. Выставили точность 12 бит (максимум)
+    hadc1.Init.ScanConvMode = DISABLE;
+    hadc1.Init.ContinuousConvMode = DISABLE;
+    hadc1.Init.DiscontinuousConvMode = DISABLE;
+    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.NbrOfConversion = 1;
+    hadc1.Init.DMAContinuousRequests = DISABLE;
+    hadc1.Init.EOCSelection = EOC_SINGLE_CONV;
+    HAL_ADC_Init(&hadc1); // инициализируем АЦП
+}
+
+uint32_t ADC_GetValue(uint32_t adc_channel)
+{
+    // Настраиваем канал
+    ADC_ChannelConfTypeDef sConfig;
+
+    sConfig.Channel = adc_channel; // номер канала, который передали в функцию
+    sConfig.Rank = 1; // его порядок в очереди. В нашем случае он один
+    sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES; // сколько тактов на сэмплирование.
+    HAL_ADC_ConfigChannel(&hadc1, &sConfig); // вызываем функцию настройки канала. Передаем ей структуру АЦП и структуру настроек канала
+
+    // Считываем значение АЦП
+    HAL_ADC_Start(&hadc1); // запускаем оцифровку сигнала
+    HAL_ADC_PollForConversion(&hadc1, 1); // дожидаемся, пока не получим результат
+
+    return HAL_ADC_GetValue(&hadc1);
+}
+
+void LedInit()
+{
     __GPIOG_CLK_ENABLE();
 
     GPIOG_Init.Pin   = GPIO_PIN_14 | GPIO_PIN_13;
     GPIOG_Init.Mode  = GPIO_MODE_OUTPUT_PP;
-    GPIOG_Init.Pull  = GPIO_PULLDOWN;
+    GPIOG_Init.Pull  = GPIO_NOPULL;
     GPIOG_Init.Speed = GPIO_SPEED_LOW;
 
     HAL_GPIO_Init(GPIOG, &GPIOG_Init);
 }
 
-void BRD_Btn_B4_Init()
+void PrintBuf()
 {
-    GPIO_InitTypeDef GPIOB_Init;
-
-    // Включаем питание и тактирование на портах B
-    __GPIOB_CLK_ENABLE();
-
-    GPIOB_Init.Pin   = GPIO_PIN_4;
-    // https://ru.wikipedia.org/wiki/Фронт_сигнала
-    // GPIOB_Init.Mode = GPIO_MODE_IT_FALLING; //- по заднему фронту (спадающий)
-    // GPIOA_Init.Mode = GPIO_MODE_IT_RISING_FALLING; // - на каждое нажатие кнопки будет сгенерировано два события, и по переднему и по заднему фронту
-    GPIOB_Init.Mode  = GPIO_MODE_IT_RISING; // событие будет сгенерировано в момент нажатия кнопки (восходящий, передний фронт)
-    GPIOB_Init.Pull  = GPIO_PULLDOWN; // подтягиваем ее к земле
-    GPIOB_Init.Speed = GPIO_SPEED_MEDIUM;
-
-    HAL_GPIO_Init(GPIOB, &GPIOB_Init); // - инициализируем порт, передаем структуру
-}
-
-void BRD_Btn_B7_Init()
-{
-    GPIO_InitTypeDef GPIOB_Init;
-
-    __GPIOB_CLK_ENABLE();
-
-    GPIOB_Init.Pin   = GPIO_PIN_7;
-    GPIOB_Init.Mode  = GPIO_MODE_IT_RISING;
-    GPIOB_Init.Pull  = GPIO_PULLDOWN;
-    GPIOB_Init.Speed = GPIO_SPEED_MEDIUM;
-
-    HAL_GPIO_Init(GPIOB, &GPIOB_Init);
-}
-
-void BRD_Btn_C3_Init()
-{
-    GPIO_InitTypeDef GPIOC_Init;
-
-    __GPIOC_CLK_ENABLE();
-
-    GPIOC_Init.Pin   = GPIO_PIN_3;
-    GPIOC_Init.Mode  = GPIO_MODE_IT_RISING;
-    GPIOC_Init.Pull  = GPIO_PULLDOWN;
-    GPIOC_Init.Speed = GPIO_SPEED_LOW;
-
-    HAL_GPIO_Init(GPIOC, &GPIOC_Init);
-}
-
-void init()
-{
-    SystemInit();
-    HAL_Init();
-    STEP_Init();
-
-    Led_Init();
-
-    BRD_Btn_B4_Init();
-    BRD_Btn_B7_Init();
-    BRD_Btn_C3_Init();
-
-    // Говорим нашему контроллеру прерываний (NVIC), что мы хотим обрабатывать прерывание EXTI4_IRQn
-    HAL_NVIC_EnableIRQ(EXTI4_IRQn);
-    HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
-    HAL_NVIC_EnableIRQ(EXTI3_IRQn);
-}
-
-int main(void)
-{
-    init();
-
-    switchState();
-
-    return 0;
-}
-
-// название обработчика прерывания аналогично прерыванию (EXTI0_IRQn) только вместо "n" - "Handler"
-void EXTI4_IRQHandler(void)
-{
-    // Тут выполняется ваш код, обработчик прерывания
-    int btn = 1;
-
-    checkBtn(btn);
-
-    char msg[25];
-    sprintf(msg, "Button: %d, count: %d", btn, index);
-    STEP_Println(msg);
-
-    // Тут выполняется ваш код, обработчик прерывания
-    HAL_NVIC_ClearPendingIRQ(EXTI4_IRQn);
-    // Тут выполняется ваш код, обработчик прерывания
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
-}
-
-void EXTI9_5_IRQHandler(void)
-{
-    int btn = 2;
-
-    checkBtn(btn);
-
-    char msg[25];
-    sprintf(msg, "Button: %d, count: %d", btn, index);
-    STEP_Println(msg);
-
-    HAL_NVIC_ClearPendingIRQ(EXTI9_5_IRQn);
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
-}
-
-void EXTI3_IRQHandler(void)
-{
-    int btn = 3;
-
-    checkBtn(btn);
-
-    char msg[25];
-    sprintf(msg, "Button: %d, index: %d", btn, index);
-    STEP_Println(msg);
-
-    HAL_NVIC_ClearPendingIRQ(EXTI3_IRQn);
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_3);
-}
-
-void switchState()
-{
-    while (1) {
-        if (index == 4) {
-            HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, 1);
-            continue;
-        } else {
-            HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, 0);
-            continue;
+    char str[128];
+    int i = 0;
+    while(!RingBuffer_IsEmpty(&rBuffer))
+    {
+        char c = RingBuffer_GetByte(&rBuffer);
+        if (c == '\r') continue;
+        str[i++ %128] = c;
+        if (c == '\n' || i>=36)
+        {
+            if (c == '\n') str[i-1] = 0;
+            else str[i] = 0;
+            i=0;
+            STEP_Println(str);
+            //MakeAction(str); // реагируем на команду
         }
     }
 }
 
-void checkBtn(int btnNum)
+void sendUART(char * cmd)
 {
-    if (code[index] == btnNum) {
-        index++;
-    } else {
-        index = 0;
+    HAL_UART_Transmit(&huart1, cmd, strlen(cmd), 100);
+}
+
+#define WIFI_SSID            "ITStep" // WiFi SSID
+#define WIFI_KEY             "stepstudent"       // WiFi key (password)
+#define THINGSPEAK_HOST_IP   "184.106.153.149"           // ThingSpeak host IP
+#define THINGSPEAK_HOST_PORT  80                         // ThingSpeak host port
+#define THINGSPEAK_API_KEY   "P5MHRH8PXY71GGJV"          // ThingSpeak API key 54584
+
+/**
+ * Available Commands: https://github.com/espressif/esp8266_at/wiki/AT_Description
+ *
+ */
+void connectWiFi(void)
+{
+    sendUART("AT\r\n");
+
+    HAL_Delay(100);
+    PrintBuf();
+
+    sendUART("AT+CWMODE=1\r\n");    // Переключение режима WiFi (1=Station, 2=AP, 3=Station+AP)
+    HAL_Delay(100);
+    PrintBuf();
+
+    char buf[200];
+    sprintf(buf, "AT+CWJAP=\"%s\",\"%s\"\r\n", WIFI_SSID, WIFI_KEY);
+    sendUART(buf);
+
+    HAL_Delay(6000);
+    PrintBuf();
+
+    sendUART("AT+CIFSR\r\n");       // Отобразить IP адрес, который получили от AP
+
+    HAL_Delay(1000);
+    PrintBuf();
+
+    sendUART("AT+CIPMUX=0\r\n");    // Выбор режима одиночных или множественных подключений (0=одиночные подключение, 1=множественные подключения)
+
+    HAL_Delay(100);
+    PrintBuf();
+
+}
+
+void disconnectWiFi(void)
+{
+    sendUART("AT+CWQAP\r\n");
+
+    HAL_Delay(2000);
+    PrintBuf();
+}
+
+void sendData(char *fieldName, const char *fieldValue)
+{
+    char buf[200];
+    sprintf(buf, "AT+CIPSTART=\"TCP\",\"%s\",%d\r\n", THINGSPEAK_HOST_IP, THINGSPEAK_HOST_PORT);
+    sendUART(buf);
+
+    HAL_Delay(1000);
+    PrintBuf();
+
+    char dbuf[2048];
+    sprintf(dbuf, "GET /update?key=%s&%s=%d\r\n", THINGSPEAK_API_KEY, fieldName, fieldValue);
+
+    // Count of data length
+    int dbufLength = 0;
+    while(dbuf[dbufLength] != '\n'){
+      dbufLength++;
     }
+    dbufLength++;
+
+    sprintf(buf, "AT+CIPSEND=%d\r\n", dbufLength);
+    sendUART(buf);
+
+    HAL_Delay(20);
+
+    sendUART(dbuf);
+
+    HAL_Delay(100);
+    //PrintBuf();
+
+    sendUART("AT+CIPCLOSE\r\n");
+    //HAL_Delay(2000);
+    //PrintBuf();
+}
+
+int main(void)
+{
+    SystemInit();
+    HAL_Init();
+    STEP_Init();
+    LedInit();
+    ADC1_Init();
+    //STEP_DBG_Osc();
+
+    UART_Init();
+
+    connectWiFi();
+
+    sendData("field1", 26);
+
+    //disconnectWiFi();
+
+
+    //STEP_Println("Connected?");
+
+    //sendUART("AT+CWLAP\r\n");
+
+    //HAL_Delay(1000);
+
+    while (1)
+    {
+        PrintBuf();
+        HAL_Delay(1000);
+    }
+}
+
+void USART1_IRQHandler()
+{
+    STEP_UART_Receive_IT(&huart1, &rBuffer);
 }
 
 void SysTick_Handler()
